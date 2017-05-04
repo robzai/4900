d103c40197ef34e59cb981304fc618bc5caf2508
Hao(Leo) Luo <robzai@hotmail.com>
2017-05-03 20:46:36 -0700

    Initial Commit

---
+++ 4900/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4900
+//
+//  Created by leo  luo on 2017-05-03.
+//  Copyright © 2017 leo  luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
