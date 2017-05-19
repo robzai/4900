5e475f88a42aefdf7e8a65346b7dcade057d0e2f
Laura Yang <laura.yang@lmdev6-lyang.hitronhub.home>
2017-05-18 22:06:11 -0700

    Added function to allow multiple image selection in FirstPostControllerView

--- 4900/FirstPageViewController.swift
+++ 4900/FirstPageViewController.swift
@@ -7,21 +7,114 @@
 //
 
 import UIKit
+import Foundation
+import Photos
+import AVKit
+import DKImagePickerController
 
-class FirstPageViewController: UIViewController {
+class FirstPageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    let pickerController = DKImagePickerController()
+    var assets: [DKAsset]?
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
+    @IBAction func showImagePicker(_ sender: Any) {
+        pickerController.defaultSelectedAssets = self.assets
+        pickerController.didSelectAssets = { [unowned self] (assets: [DKAsset]) in
+            self.assets = assets
+            
+            // Yaoyao: Get UIImage data and send data to server
+            // ps: use line 35 image variable
+            for asset in self.assets! {
+                asset.fetchOriginalImageWithCompleteBlock({ (image, info) in
+                    let imageData = UIImageJPEGRepresentation(image!, 0)
+                    //                    self.env64string = imageData!.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+                    //                    self.env64strings[env64string] = image // the env64strings is a dictionary.
+                })
+            }
+            
+            self.collectionView?.reloadData()
+        }
+        
+        if UI_USER_INTERFACE_IDIOM() == .pad {
+            pickerController.modalPresentationStyle = .formSheet
+        }
+        
+        self.present(pickerController, animated: true) {}
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(white: 0.9 , alpha: 1)
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let rightBarButton = UIBarButtonItem(title: "Post", style: UIBarButtonItemStyle.plain, target: self, action: Selector("Post"))
+        self.navigationItem.rightBarButtonItem = rightBarButton
+        self.navigationItem.rightBarButtonItem?.tintColor = UIColor.white
+        
+        let leftBarButton = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.plain, target: self, action: Selector("Cancel"))
+        self.navigationItem.leftBarButtonItem = leftBarButton
+        self.navigationItem.leftBarButtonItem?.tintColor = UIColor.white
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return self.assets?.count ?? 0
     }
     
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let asset = self.assets![indexPath.row]
+        var cell: UICollectionViewCell?
+        var imageView: UIImageView?
+        
+        if asset.isVideo {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellVideo", for: indexPath)
+            imageView = cell?.contentView.viewWithTag(1) as? UIImageView
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellImage", for: indexPath)
+            imageView = cell?.contentView.viewWithTag(1) as? UIImageView
+        }
+        
+        if let cell = cell, let imageView = imageView {
+            let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+            let tag = indexPath.row + 1
+            cell.tag = tag
+            asset.fetchImageWithSize(layout.itemSize.toPixel(), completeBlock: { image, info in
+                if cell.tag == tag {
+                    imageView.image = image
+                }
+            })
+        }
+        return cell!
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let asset = self.assets![indexPath.row]
+        asset.fetchAVAssetWithCompleteBlock { (avAsset, info) in
+            DispatchQueue.main.async(execute: { () in
+                self.playVideo(avAsset!)
+            })
+        }
+    }
 
+    func playVideo(_ asset: AVAsset) {
+        let avPlayerItem = AVPlayerItem(asset: asset)
+        
+        let avPlayer = AVPlayer(playerItem: avPlayerItem)
+        let player = AVPlayerViewController()
+        player.player = avPlayer
+        
+        avPlayer.play()
+        
+        self.present(player, animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
